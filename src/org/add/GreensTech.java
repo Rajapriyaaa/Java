46905c9b58c1cce7d4edb430a66142b1dad21bbf
Rajapriyaaa <116809029+Rajapriyaaa@users.noreply.github.com>
2022-10-31 13:07:34 +0530

    MY Task

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,38 @@
+package org.add;
+
+public class GreensTech {
+	//method creation 1
+	public void greensOmr() {
+		System.out.println("greens Omr Chennai");
+		}
+	//method creation 2
+	public void greensAdayar() {
+		System.out.println("greens Adayar Chennai");
+		
+	}
+// method creation 3
+	public void greensTambaram() {
+		System.out.println("greeens Thambaram Chennai");
+		
+	}
+	//method creation 4
+	public void greensVelacherry() {
+		System.out.println("greens Velacherryna Chennai");
+		
+	}
+	// method creation 5
+	public void greensAnnaNagar() {
+		System.out.println("greens AnnaNagar Chennai");
+		
+	}
+	public static void main(String[] args) {
+		// object creation
+		GreensTech g=new GreensTech();
+		g.greensOmr();
+		g.greensAdayar();
+		g.greensAnnaNagar();
+		g.greensTambaram();
+		g.greensVelacherry();
+		
+	}
+}
